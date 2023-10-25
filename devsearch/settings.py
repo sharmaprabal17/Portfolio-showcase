e52244e31535306610e5605edabb12ee6b34a855
Prabal Sharma <sharmaprabal17@gmail.com>
2023-10-25 15:21:21 +0530

    fix

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -125,8 +125,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587 #tls port
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'mystinkybutt17@gmail.com'
-EMAIL_HOST_PASSWORD = 'mothercoconuts'
+EMAIL_HOST_USER = 'YOUR_HOST_EMAIL'
+EMAIL_HOST_PASSWORD = 'PASSWORD_FOR_IT'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
